b360c52560d6923818bcdcfb321ec20e8de756d2
sntshmohapatra2020 <sntshmohapatra2020@gmail.com>
2026-02-11 19:59:37 +0530

    all test cases passed

--- app/test/test_users.py
+++ app/test/test_users.py
@@ -26,3 +26,13 @@ def test_change_password_success(test_users):
                           )
     assert response.status_code == status.HTTP_204_NO_CONTENT
     
+def test_change_password_invalid_current_password(test_users):
+    response = client.put("/users/password", 
+                          json={
+                              "password": "wrongpassword",
+                                "new_password": "newpassword"
+                                }
+                          )
+    assert response.status_code == status.HTTP_401_UNAUTHORIZED
+    assert response.json() == {'detail': 'Error on password change'}
+
